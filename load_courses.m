1e82d0575a618ea94ef4fcd51eb381659f2cbed5
DrTBaylor <89058316+DrTBaylor@users.noreply.github.com>
2021-08-23 17:50:46 -0500

    Initial

---
+++ load_courses.m
@@ -0,0 +1,18 @@
+
+
+
+allowed{1}='EGR 1301 Intro 1 Pre1'
+allowed{2}='EGR 1302 Intro 2 Pre2'
+allowed{3}='ELC 2320 Circuits';
+allowed{4}='ELC 4351 DSP';
+
+list_course{1}='EGR 1301 Intro to EGR 1';
+list_courses{2}='EGR 1302 Intro to EGR 2';
+list_courses{3}='ELC 2320 Intro to circuits';
+list_courses{3}='ELC 4351 Digital Signal Processing';
+
+
+
+
+
+
